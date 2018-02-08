a4bcfe0b49747e39c48c4f4dd744accbb9c68a58
thukuwanjiku <mikedegr809@gmail.com>
2018-02-08 14:49:56 +0300

    Vertical-align:middle for material account icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -132,7 +132,7 @@
                             <li><a href="{{ route('newpost') }}"><i class="fas fa-edit"> New Article</i></a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                                    <i class="material-icons">account_circle</i>
+                                    <i class="material-icons" style="vertical-align: middle;">account_circle</i> <span class="caret"></span>
                                 </a>
 
                                 <ul class="dropdown-menu">
