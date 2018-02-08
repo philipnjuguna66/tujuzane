7c610e3c9fbaa9b4e7793f817cf8a71374961e64
thukuwanjiku <mikedegr809@gmail.com>
2018-02-08 16:27:35 +0300

    Revert changes to config vars

--- config/database.php
+++ config/database.php
@@ -19,7 +19,7 @@ return [
     |
     */
 
-    'default' => /*env('DB_CONNECTION', 'pgsql')*/'pgsql',
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
